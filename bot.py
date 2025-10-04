19af373f1cecdf21ea454df31f4c0d8111320f73
greejjddg09 <127425061+greejjddg09@users.noreply.github.com>
2025-10-04 11:29:46 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,6 +16,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
 
 from pyowm import OWM
 from pyowm.utils.config import get_default_config
